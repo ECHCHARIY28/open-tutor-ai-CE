8fe09aff5400b59c3b0f4414a37ffd07f9b7165b
Lahcen <echcharyilahcen@gmail.com>
2026-04-01 18:49:43 +0100

    fix: creation of teacher study pages + allow admin to access teacher page

--- src/routes/teacher/settings/+page.svelte
+++ src/routes/teacher/settings/+page.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <div class="w-full h-full min-h-[60vh] border-2 border-dashed border-gray-300 rounded-xl flex items-center justify-center bg-gray-50">
-	<h1 class="text-2xl text-gray-400 font-bold">Page: Paramètres (Khawya)</h1>
+	<h1 class="text-2xl text-gray-400 font-bold">Page: Paramètres</h1>
 </div>
